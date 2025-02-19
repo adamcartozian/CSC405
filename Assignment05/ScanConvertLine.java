15af2efa07b8b88caf5d8e858da492a68edb3b7c
adamcartozian <98410927+adamcartozian@users.noreply.github.com>
2025-02-18 22:19:47 -0800

    Made the color shift correct

--- Assignment05/ScanConvertLine.java
+++ Assignment05/ScanConvertLine.java
@@ -16,9 +16,10 @@ public class ScanConvertLine extends ScanConvertAbstract {
                     //int[] plottedcolor = c0rgb;
 
                     double r = c0rgb[0];
-                    double g = c0rgb[1];
-                    double b = c0rgb[2];
-                    int x = x0;
+                    double g = c0rgb[2];
+                    double b = c0rgb[1];
+                    int y = y0;
+                    double t = 0;
                     
 
                     int dx = Math.abs(x1 - x0);
@@ -33,39 +34,35 @@ public class ScanConvertLine extends ScanConvertAbstract {
                 
                     while (true) {
                     // Plot the current point
+                        
                         framebuffer[0][y0][(int)Math.round(x0)] = (int)r;
                         framebuffer[1][y0][(int)Math.round(x0)] = (int)b;
                         framebuffer[2][y0][(int)Math.round(x0)] = (int)g;
-                        if(x <= x1){
-                            double multiplier = (double)(x - x0) / (double)(x1 - x0);
-                            if (r > c1rgb[0]){
-                                r -= (multiplier * (r - c1rgb[0]));
-                            } else if (c1rgb[0] > r){
-                                r += (multiplier * (c1rgb[0] - r));
-                            }
-                
-                            if (g > c1rgb[1]){
-                                g -= (multiplier * (g - c1rgb[1]));
-                            } else if (c1rgb[1] > g){
-                                g += (multiplier * (c1rgb[1] - g));
-                            }
-                            
-                            if (b > c1rgb[2]){
-                                b -= (multiplier * (b - c1rgb[2]));
-                            } else if (c1rgb[2] > b){
-                                b += (multiplier * (c1rgb[2] - b));
-                            }
-                
-                            r = (r > 1.0) ? 1.0 : (r < 0.0) ? 0.0 : r;
-                            g = (g > 1.0) ? 1.0 : (g < 0.0) ? 0.0 : g;
-                            b = (b > 1.0) ? 1.0 : (b < 0.0) ? 0.0 : b;  
-                            x++;
+
+
+                    // increment color
+                        if (r > c1rgb[0]){
+                            r--;
+                        } else if (c1rgb[0] > r){
+                            r++;
                         }
-                        /*if (plottedcolor != c1rgb){
-                            plottedcolor[0] = plottedcolor[0] - 10;
-                            plottedcolor[1] = plottedcolor[1] + 10;
-                            plottedcolor[2] = plottedcolor[2] + 10;
-                        }*/
+            
+                        if (g > c1rgb[2]){
+                            g--;
+                        } else if (c1rgb[2] > g){
+                            g++;
+                        }
+                        
+                        if (b > c1rgb[1]){
+                            b--;
+                        } else if (c1rgb[1] > b){
+                            b++;
+                        }
+            
+                        r = (r > 255) ? 255 : (r < 0.0) ? 0.0 : r;
+                        g = (g > 255) ? 255 : (g < 0.0) ? 0.0 : g;
+                        b = (b > 255) ? 255 : (b < 0.0) ? 0.0 : b;  
+                    
                     // If we reach the endpoint, break the loop
                         if (x0 == x1 && y0 == y1) {
                         break;
